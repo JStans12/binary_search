d84542f1d684f6cd010d93363255a22c949209f2
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-08-23 20:39:15 -0600

    somehow figured out sort

--- bst/lib/bst.rb
+++ bst/lib/bst.rb
@@ -2,53 +2,70 @@ require "pry"
 
 class BinarySearchTree
   attr_reader :root
+  attr_accessor :sorted
 
-  def initialize(root = nil)
+  def initialize(root = nil, sorted = [])
     @root = root
+    @sorted = sorted
   end
 
-  def insert_node(insert_score, insert_movie, current_node = @root, depth = 1)
+  def insert_node(input_score, input_movie, current_node = @root, depth = 1)
+
+    # this commented out bit is a refactored version of insert_node
+    # it returns more than just depth
+
+    # return 0 && @root = Node.new(input_score, input_movie, 0) if @root == nil
+    #
+    # return nil if input_score == current_node.score
+    #
+    # return depth && current_node.left = Node.new(input_score, input_movie, depth) if input_score < current_node.score && current_node.left == nil
+    #
+    # self.insert_node(input_score, input_movie, current_node.left, depth + 1) if input_score < current_node.score && current_node.left != nil
+    #
+    # return depth && current_node.right = Node.new(input_score, input_movie, depth) if input_score > current_node.score && current_node.right == nil
+    #
+    # self.insert_node(input_score, input_movie, current_node.right, depth + 1) if input_score > current_node.score && current_node.right != nil
 
     if @root == nil
-      @root = Node.new(insert_score, insert_movie, 0)
+      @root = Node.new(input_score, input_movie, 0)
       return 0
     else
 
-      return nil if insert_score == current_node.score
+      return nil if input_score == current_node.score
 
-      if insert_score < current_node.score
+      if input_score < current_node.score
         if current_node.left == nil
-          current_node.left = Node.new(insert_score, insert_movie, depth)
+          current_node.left = Node.new(input_score, input_movie, depth)
           return depth
         else
-          self.insert_node(insert_score, insert_movie, current_node.left, depth + 1)
+          self.insert_node(input_score, input_movie, current_node.left, depth + 1)
         end
 
       else
         if current_node.right == nil
-          current_node.right = Node.new(insert_score, insert_movie, depth)
+          current_node.right = Node.new(input_score, input_movie, depth)
           return depth
         else
-          self.insert_node(insert_score, insert_movie, current_node.right, depth + 1)
+          self.insert_node(input_score, input_movie, current_node.right, depth + 1)
         end
       end
     end
   end # end insert_node
 
-  def include?(check_score, current_node = @root)
+  def include?(input_score, current_node = @root)
 
     if current_node == nil
       return false
     else
 
-      if check_score == current_node.score
+      if input_score == current_node.score
         return true
 
-      elsif check_score < current_node.score
-        self.include?(check_score, current_node.left)
+      elsif input_score < current_node.score
+        self.include?(input_score, current_node.left)
 
-      elsif check_score > current_node.score
-        self.include?(check_score, current_node.right)
+      elsif input_score > current_node.score
+        self.include?(input_score, current_node.right)
 
       else
         return false
@@ -56,25 +73,64 @@ class BinarySearchTree
     end
   end # end include?
 
-  def depth_of(check_score, current_node = @root)
+  def depth_of(input_score, current_node = @root)
 
     if current_node == nil
       return nil
     else
 
-      if check_score == current_node.score
+      if input_score == current_node.score
         return current_node.depth
 
-      elsif check_score < current_node.score
-        self.depth_of(check_score, current_node.left)
+      elsif input_score < current_node.score
+        self.depth_of(input_score, current_node.left)
 
-      elsif check_score > current_node.score
-        self.depth_of(check_score, current_node.right)
+      elsif input_score > current_node.score
+        self.depth_of(input_score, current_node.right)
 
       else
         return nil
       end
     end
   end # end depth_of
+
+  def max(current_node = @root)
+
+    if current_node.right == nil
+      return {current_node.movie => current_node.score}
+    else
+      max(current_node.right)
+    end
+
+  end # end max
+
+  def min(current_node = @root)
+
+    if current_node.left == nil
+      return {current_node.movie => current_node.score}
+    else
+      min(current_node.left)
+    end
+
+  end # end min
+
+  def sort(current_node = @root)
+
+    if current_node.left != nil && @sorted.include?({current_node.left.movie => current_node.left.movie}) == false
+      sort(current_node.left)
+
+      if @sorted.include?({current_node.movie => current_node.score}) == false
+        @sorted << {current_node.movie => current_node.score}
+      end
+
+    else
+      @sorted << {current_node.movie => current_node.score}
+    end
+
+    if current_node.right != nil && @sorted.include?({current_node.right.movie => current_node.right.movie}) == false
+      sort(current_node.right)
+    end
+
+  end # end sort
 
 end # end BinarySearchTree
--- bst/test/bst_test.rb
+++ bst/test/bst_test.rb
@@ -2,6 +2,7 @@ require './lib/bst.rb'
 require './lib/node.rb'
 require "minitest/autorun"
 require "minitest/pride"
+require 'pry'
 
 class BinarySearchTest < Minitest::Test
 
@@ -14,12 +15,28 @@ class BinarySearchTest < Minitest::Test
     assert_equal tree.root.score, 50
   end
 
+  def test_if_new_insert_already_exists
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+
+    assert_equal tree.insert_node(50, "james bod"), nil
+  end
+
   def test_insert_on_left
     tree = BinarySearchTree.new
     tree.insert_node(50, "james bond")
     tree.insert_node(45, "avitar")
 
     assert_equal tree.root.left.score, 45
+  end
+
+  def test_insert_two_on_left
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.insert_node(45, "avitar")
+    tree.insert_node(11, "star wars")
+
+    assert_equal tree.root.left.left.score, 11
   end
 
   def test_insert_on_right
@@ -114,6 +131,116 @@ class BinarySearchTest < Minitest::Test
     assert_equal tree.depth_of(53), 2
     assert_equal tree.depth_of(21), nil
     assert_equal tree.depth_of(88), nil
+  end
+
+  def test_max_at_root
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+
+    assert_equal tree.max, {"james bond" => 50}
+  end
+
+  def test_max_full
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.insert_node(45, "avitar")
+    tree.insert_node(55, "dig")
+    tree.insert_node(60, "the hobbit")
+    tree.insert_node(11, "star wars")
+    tree.insert_node(85, "mad max")
+    tree.insert_node(53, "i robot")
+
+    assert_equal tree.max, {"mad max" => 85}
+  end
+
+  def test_min_at_root
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+
+    assert_equal tree.max, {"james bond" => 50}
+  end
+
+  def test_min_full
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.insert_node(45, "avitar")
+    tree.insert_node(55, "dig")
+    tree.insert_node(60, "the hobbit")
+    tree.insert_node(11, "star wars")
+    tree.insert_node(85, "mad max")
+    tree.insert_node(53, "i robot")
+
+    assert_equal tree.min, {"star wars" => 11}
+  end
+
+  def test_sort_for_one_node
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.sort
+
+    assert_equal tree.sorted, [{"james bond" => 50}]
+  end
+
+  def test_sort_to_the_right
+    tree = BinarySearchTree.new
+    tree.insert_node(45, "avitar")
+    tree.insert_node(50, "james bond")
+    tree.sort
+
+    assert_equal tree.sorted, [{"avitar" => 45}, {"james bond" => 50}]
+  end
+
+  def test_sort_left_backtrack
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.insert_node(45, "avitar")
+    tree.insert_node(11, "star wars")
+    tree.sort
+
+    assert_equal tree.sorted, [{"star wars" => 11}, {"avitar" => 45}, {"james bond" => 50}]
+  end
+
+  def test_sort_left_and_right
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.insert_node(45, "avitar")
+    tree.insert_node(55, "dig")
+    tree.sort
+
+    assert_equal tree.sorted, [{"avitar" => 45}, {"james bond" => 50}, {"dig" => 55}]
+  end
+
+  def test_sort_does_this_actually_work_wtf
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.insert_node(45, "avitar")
+    tree.insert_node(55, "dig")
+    tree.insert_node(60, "the hobbit")
+    tree.insert_node(11, "star wars")
+    tree.insert_node(85, "mad max")
+    tree.insert_node(53, "i robot")
+    tree.sort
+
+    assert_equal tree.sorted, [{"star wars" => 11}, {"avitar" => 45}, {"james bond" => 50}, {"i robot" => 53}, {"dig" => 55}, {"the hobbit" => 60}, {"mad max" => 85}]
+  end
+
+  def test_sort_this_cannot_possibly_be_the_answer
+    tree = BinarySearchTree.new
+    tree.insert_node(50, "james bond")
+    tree.insert_node(45, "avitar")
+    tree.insert_node(55, "dig")
+    tree.insert_node(60, "the hobbit")
+    tree.insert_node(11, "star wars")
+    tree.insert_node(85, "mad max")
+    tree.insert_node(53, "i robot")
+    tree.insert_node(12, "sharknado")
+    tree.insert_node(54, "fire walk with me")
+    tree.insert_node(59, "cry baby")
+    tree.insert_node(47, "the shining")
+    tree.insert_node(46, "momento")
+    tree.sort
+
+    assert_equal tree.sorted, [{"star wars" => 11}, {"sharknado" => 12}, {"avitar" => 45}, {"momento" => 46}, {"the shining" => 47}, {"james bond" => 50}, {"i robot" => 53}, {"fire walk with me" => 54}, {"dig" => 55}, {"cry baby" => 59}, {"the hobbit" => 60}, {"mad max" => 85}]
   end
 
 end # end tests
